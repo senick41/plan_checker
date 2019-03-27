b0881368d613edb5e35d05d1023149403bc56667
Scott Senick <scott.senick@gmail.com>
2019-03-27 13:43:10 -0700

    Add project files.

---
+++ SMSPlanCheck/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ESAPIX.Bootstrapper;
+using SMSPlanCheck.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ESAPIX.Common;
+
+namespace SMSPlanCheck
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string[] args = e.Args;
+            base.OnStartup(e);
+            var bs = new AppBootstrapper<MainView>(() => { return VMS.TPS.Common.Model.API.Application.CreateApplication(); });
+            //You can use the following to load a context (for debugging purposes)
+            //args = ContextIO.ReadArgsFromFile(@"context.txt");
+            //Might disable (uncomment) for plugin mode
+            //bs.IsPatientSelectionEnabled = false;
+            bs.Run(args);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            AppComThread.Instance.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
